bfdaa43dee002f464e53f490b369f18a1649cd2a
Zack Smith <zack@puppetlabs.com>
2016-05-13 11:46:12 -0700

    Fix examples

--- manifests/dynamic_scoping_erb_templates_outer.pp
+++ manifests/dynamic_scoping_erb_templates_outer.pp
@@ -1,4 +1,4 @@
 class broken::dynamic_scoping_erb_templates_outer { 
  $var = 'dynamic scoping with current parser'
- include catalog_preview::examples::dynamic_scoping_erb_templates_inner
+ include broken::dynamic_scoping_erb_templates_inner
 }
